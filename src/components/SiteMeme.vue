ae96a75e98f4ab286796a370e26ae0f361b63947
Lee Ravenberg <lee@ravenberg.dev>
2024-11-19 00:01:57 +0100

    fix: safari clipboard write operation

--- src/components/SiteMeme.vue
+++ src/components/SiteMeme.vue
@@ -1,16 +1,18 @@
 <script setup>
-import { ref } from "vue";
+import { onMounted, ref } from "vue";
 
 const props = defineProps({
   name: String,
 })
 
-const alt = props.name.replace(/-([a-z])/g, (g) => g[1].toUpperCase())
+const alt = props.name.split('-')
+  .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+  .join(' ');
 const memeRef = ref(null)
 const copied = ref(false)
+const clipboardItem = ref(null)
 
-// copy image to clipboard using the clipboard API
-const copyImage = () => {
+const convertImageToClipboardItem = () => {
   const img = memeRef.value
   const canvas = document.createElement("canvas")
   const ctx = canvas.getContext("2d")
@@ -21,21 +23,28 @@ const copyImage = () => {
     canvas.height = imgElement.height
     ctx.drawImage(imgElement, 0, 0)
     canvas.toBlob((blob) => {
-      const item = new ClipboardItem({ "image/png": blob })
-      navigator.clipboard.write([item])
+      clipboardItem.value = new ClipboardItem({ "image/png": blob })
     })
   }
+}
 
+const copyImage = () => {
+  navigator.clipboard.write([clipboardItem.value])
   copied.value = true
   setTimeout(() => {
     copied.value = false
   }, 3000)
 }
+
+onMounted(() => {
+  // The image need to be converted beforehand so it can write to clipboard within the click handler stack.
+  convertImageToClipboardItem()
+})
 </script>
 
 <template>
   <li class="px-4 relative">
-    <button class="absolute right-7 bottom-2 rounded py-2 px-1 bg-gray-800 text-gray-200 opacity-70 hover:opacity-100 transition" @pointerup="copyImage">
+    <button class="absolute right-7 bottom-2 rounded py-2 px-1 bg-gray-800 text-gray-200 opacity-70 hover:opacity-100 transition" @click="copyImage">
       <span v-if="copied">✅ Copied</span>
       <span v-else>📋 Copy</span>
     </button>
